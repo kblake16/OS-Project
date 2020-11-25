a2749c6d1009b3e7d05fa2bedefaeff499d533c0
Keisha Morris <keishamorris@Keishas-MacBook-Pro.local>
2020-11-25 17:22:40 -0500

    Edited VM.html and Create VM.js

--- VM.js
+++ VM.js
@@ -17,32 +17,66 @@ window.onload = function()
     */
 
     let PhysicalSize = document.getElementById("pps");
-    let OffsetBits = document.getElementById("ofb");
-    let VirtualMemory = document.getElementById("vms");
+    let Offset = document.getElementById("ofb");
+    let VM = document.getElementById("vms");
     let TLB = document.getElementById("tlb");
 
     let reset = document.getElementById("reset");
     let submit = document.getElementById("submit");
 
-    var table = document.getElementById("myTable");
+    var VMtable = document.getElementById("VirtualMemoryTable");
     var TLBtable = document.getElementById("TLBTable");
-    var PagingTable = document.getElementById("myTable");
-    //var Instruction breakdown = document.getElementById("myTable");
+    var Pagingtable = document.getElementById("Pagetable");
+    var PhysicalMemorytable = document.getElementById("PhysicalMemoryTable");
 
     submit.onclick = function createTable(){
         console.log("clicked");
-        console.log(TLB.value);
-        var row = 0;
-        for (var i = parseInt(TLB.value) -1; i>=0;i--)
+
+        console.log
+
+        var div = 2**parseInt(Offset.value);
+        var tlbVal = parseInt(TLB.value);
+        var offsetVal = parseInt(Offset.value);
+        var vmVal = parseInt(VM.value);
+        var physVal = parseInt(PhysicalSize.value);
+
+        //TLB table
+        var rowNum = 1;
+        for (var i = tlbVal -1; i>=0;i--)
         {
-            var row = TLBtable.insertRow(row);
+            var row = TLBtable.insertRow(rowNum);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             cell1.innerHTML = i;
             row++;
         }
 
-        var row = 0;
+        // Display and calculate Offset and Page bits
 
+        var InstructionLength = Math.log2(vmVal);
+        var row = VMtable.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = InstructionLength - offsetVal + " bits";
+        cell2.innerHTML = offsetVal + " bits";
+
+        //submit.onclick = null;
+    }
+
+    reset.onclick = function clearTable(){
+        console.log("clicked");
+
+        var rowCount = TLBtable.rows.length - 1;
+        console.log(rowCount);
+        for( var i = rowCount; i > 0; i--)
+        {
+            TLBtable.deleteRow(i);
+        }
+
+        VMtable.deleteRow(1);
+
+       //submit.onclick = function createTable();
     }
 }
+
+//function toHex
